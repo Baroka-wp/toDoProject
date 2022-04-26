05209401e3238484fb65c6e7eeb217b6e719995d
baroka <birotori@gmail.com>
2022-04-26 07:52:29 +0100

    Add delete function

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ ToDoList.displayTaskList();
 let list = new ToDoList
 const addToDo = document.querySelector('.addToDo')
 const form = document.querySelector('form')
+const taskList = document.querySelectorAll('.taskList li p')
+//const grabIcone = document.querySelector('.fa-ellipsis-vertical')
+
 
 form.addEventListener('keypress', (e) => {
   if(e.key === "Enter") {
@@ -14,4 +17,21 @@ form.addEventListener('keypress', (e) => {
     ToDoList.addTask({description: addToDo.value});
     addToDo.value = ''
   }
+})
+
+taskList.forEach(task => {
+  task.addEventListener('click', (e) => {
+      const element = e.target.parentElement.querySelector('i');
+      element.classList.toggle('fa-trash-can')
+      element.classList.toggle('fa-ellipsis-vertical')
+      task.classList.toggle('active');
+
+      document.querySelector('.fa-trash-can')
+                .addEventListener('click', (e) => {
+          const index = e.target.parentElement.classList[0]
+          const id = parseInt(index);
+          ToDoList.deleteTask(id);
+          e.target.parentElement.remove();
+      })
+  })
 })
--- src/toDoList.js
+++ src/toDoList.js
@@ -13,26 +13,39 @@ export default class ToDoList {
   static addTask(newTask) {
     const taskList = ToDoList.getAllTask();
     const tasks = document.querySelector('.taskList')
-
     newTask.id = taskList.length + 1 || 1
     taskList.push(newTask);
     localStorage.setItem('taskList', JSON.stringify(taskList));
-
     const li = document.createElement('li');
+    li.classList.add(+newTask.id)
     li.innerHTML = `
         <input class="checkbox" type="checkbox">
         <p>${newTask.description}</p>
         <i class="fa-solid fa-ellipsis-vertical"></i>
     `;
-    tasks.prepend(li)
+    tasks.prepend(li);
+    li.addEventListener('click', (e) => {
+      console.log("coucouc")
+        const element = e.target.parentElement.querySelector('i');
+        element.classList.toggle('fa-trash-can')
+        element.classList.toggle('fa-ellipsis-vertical')
+        li.classList.toggle('active')
+    })
   }
 
+  static deleteTask(index) {
+    console.log(index)
+    const taskList = ToDoList.getAllTask();
+    const newTaskList = taskList.filter((task) => task.id !== index);
+    localStorage.setItem('taskList', JSON.stringify(newTaskList));
+  }
 
   static displayTaskList() {
     const taskList = ToDoList.getAllTask();
     taskList.sort((a,b) =>{ return b.id - a.id })
             .forEach(task => {
       const li = document.createElement('li');
+      li.classList.add(+task.id)
       li.innerHTML = `
           <input class="checkbox" type="checkbox">
           <p>${task.description}</p>
