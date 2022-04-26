2bc6a148b86ed3c93cbf1031e9409f49c2af473c
baroka <birotori@gmail.com>
2022-04-26 08:07:30 +0100

    linters errors fixed

--- src/index.js
+++ src/index.js
@@ -3,35 +3,33 @@ import ToDoList from './ToDoList.js';
 
 ToDoList.displayTaskList();
 
-
-let list = new ToDoList
-const addToDo = document.querySelector('.addToDo')
-const form = document.querySelector('form')
-const taskList = document.querySelectorAll('.taskList li p')
-//const grabIcone = document.querySelector('.fa-ellipsis-vertical')
-
+const list = new ToDoList();
+const addToDo = document.querySelector('.addToDo');
+const form = document.querySelector('form');
+const taskList = document.querySelectorAll('.taskList li p');
+// const grabIcone = document.querySelector('.fa-ellipsis-vertical')
 
 form.addEventListener('keypress', (e) => {
-  if(e.key === "Enter") {
+  if (e.key === 'Enter') {
     e.preventDefault();
-    ToDoList.addTask({description: addToDo.value});
-    addToDo.value = ''
+    ToDoList.addTask({ description: addToDo.value });
+    addToDo.value = '';
   }
-})
+});
 
-taskList.forEach(task => {
+taskList.forEach((task) => {
   task.addEventListener('click', (e) => {
-      const element = e.target.parentElement.querySelector('i');
-      element.classList.toggle('fa-trash-can')
-      element.classList.toggle('fa-ellipsis-vertical')
-      task.classList.toggle('active');
+    const element = e.target.parentElement.querySelector('i');
+    element.classList.toggle('fa-trash-can');
+    element.classList.toggle('fa-ellipsis-vertical');
+    task.classList.toggle('active');
 
-      document.querySelector('.fa-trash-can')
-                .addEventListener('click', (e) => {
-          const index = e.target.parentElement.classList[0]
-          const id = parseInt(index);
-          ToDoList.deleteTask(id);
-          e.target.parentElement.remove();
-      })
-  })
-})
+    document.querySelector('.fa-trash-can')
+      .addEventListener('click', (e) => {
+        const index = e.target.parentElement.classList[0];
+        const id = parseInt(index);
+        ToDoList.deleteTask(id);
+        e.target.parentElement.remove();
+      });
+  });
+});
--- src/toDoList.js
+++ src/toDoList.js
@@ -1,7 +1,7 @@
 export default class ToDoList {
   constructor() {
-    this.id = "";
-    this.description = "";
+    this.id = '';
+    this.description = '';
     this.completed = false;
   }
 
@@ -12,12 +12,12 @@ export default class ToDoList {
 
   static addTask(newTask) {
     const taskList = ToDoList.getAllTask();
-    const tasks = document.querySelector('.taskList')
-    newTask.id = taskList.length + 1 || 1
+    const tasks = document.querySelector('.taskList');
+    newTask.id = taskList.length + 1 || 1;
     taskList.push(newTask);
     localStorage.setItem('taskList', JSON.stringify(taskList));
     const li = document.createElement('li');
-    li.classList.add(+newTask.id)
+    li.classList.add(+newTask.id);
     li.innerHTML = `
         <input class="checkbox" type="checkbox">
         <p>${newTask.description}</p>
@@ -25,16 +25,16 @@ export default class ToDoList {
     `;
     tasks.prepend(li);
     li.addEventListener('click', (e) => {
-      console.log("coucouc")
-        const element = e.target.parentElement.querySelector('i');
-        element.classList.toggle('fa-trash-can')
-        element.classList.toggle('fa-ellipsis-vertical')
-        li.classList.toggle('active')
-    })
+      console.log('coucouc');
+      const element = e.target.parentElement.querySelector('i');
+      element.classList.toggle('fa-trash-can');
+      element.classList.toggle('fa-ellipsis-vertical');
+      li.classList.toggle('active');
+    });
   }
 
   static deleteTask(index) {
-    console.log(index)
+    console.log(index);
     const taskList = ToDoList.getAllTask();
     const newTaskList = taskList.filter((task) => task.id !== index);
     localStorage.setItem('taskList', JSON.stringify(newTaskList));
@@ -42,18 +42,16 @@ export default class ToDoList {
 
   static displayTaskList() {
     const taskList = ToDoList.getAllTask();
-    taskList.sort((a,b) =>{ return b.id - a.id })
-            .forEach(task => {
-      const li = document.createElement('li');
-      li.classList.add(+task.id)
-      li.innerHTML = `
+    taskList.sort((a, b) => b.id - a.id)
+      .forEach((task) => {
+        const li = document.createElement('li');
+        li.classList.add(+task.id);
+        li.innerHTML = `
           <input class="checkbox" type="checkbox">
           <p>${task.description}</p>
           <i class="fa-solid fa-ellipsis-vertical"></i>
       `;
-      document.querySelector('.taskList').appendChild(li);
-    })
+        document.querySelector('.taskList').appendChild(li);
+      });
   }
-
-
 }
