333e19b6051633ed04c53e8d6fce7a4193ae524d
baroka <birotori@gmail.com>
2022-05-05 09:58:52 +0100

    Refactoring the code

--- src/tests/addTask.test.js
+++ src/tests/addTask.test.js
@@ -1,22 +1,20 @@
 import addTask from '../modules/addTask.js';
-
 const taskList = [{ id: 1, description: 'My old task', completed: false }];
+const newTask = { description: 'My new task' };
+
 describe('Add new task to storage', () => {
   test('chech new task added properly in taskList', () => {
-    const newTask = { description: 'My new task' };
     addTask(taskList, newTask);
     expect(taskList.pop().description).toBe('My new task');
   });
   test('check completed is false by default', () => {
-    const newTask = { description: 'My new task' };
     addTask(taskList, newTask);
     expect(taskList.pop().completed).toBe(false);
   });
   test('check Id is order from 1', () => {
-    const newTask = { description: 'new task' };
     addTask(taskList, newTask);
     addTask(taskList, newTask);
-    expect(taskList.pop().id).toBe(3);
-    expect(taskList.shift().id).toBe(1);
+    expect(taskList[2].id).toBe(3);
+    expect(taskList[0].id).toBe(1);
   });
 });
